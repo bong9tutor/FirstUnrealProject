5196d8d2e6ab360e213ed0e98d4b5891f0a9f111
bong9tutor <bong9tutor@gmail.com>
2026-01-17 00:31:21 +0900

    Add first-person character with XBot skeletal mesh
    - Configure default map to StartMap in DefaultEngine.ini
    - Create BP_FpCharacter blueprint for player character
    - Import Mixamo XBot skeletal mesh with materials and physics asset
    - Implement FpCharacter C++ class with basic character setup

---
+++ Source/FirstUnrealProject/Character/FpCharacter.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FpCharacter.h"
+
+AFpCharacter::AFpCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AFpCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AFpCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFpCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/FirstUnrealProject/Character/FpCharacter.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FpCharacter.generated.h"
+
+UCLASS()
+class FIRSTUNREALPROJECT_API AFpCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AFpCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
